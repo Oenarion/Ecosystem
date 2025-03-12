8dbd87af41efe9b91a884807d0dcb56dbfa9c46c
Lorenzo Galfano <41635328+Oenarion@users.noreply.github.com>
2025-03-12 17:09:02 +0100

    add working friction
    Now movers experiment friction when hitting the floor

--- Forces/main.py
+++ Forces/main.py
@@ -22,21 +22,49 @@ def update_screen(screen: pygame.display, movers: list):
         rect = pygame.Rect(position.x, position.y, w_width, w_height)
         pygame.draw.rect(screen, color, rect)
 
+def compute_friction(mover):
+    """
+    Computes friction, where F = -1*μ*N*v.
+
+    Args:
+        - mover -> the mover object which we need to apply friction to
+
+    Returns the friction force.
+    """
+
+    c = 0.7
+    normal = 1
+
+    vel = mover.get_velocity().copy()
+    
+    if vel.length_squared() == 0:  # No friction if already stopped
+        return pygame.Vector2(0, 0)
+
+    print(f"curr mover velocity: {vel}")
+    vel *= -1  
+    vel.normalize_ip() 
+    vel *= c * normal  
+
+    print(f"computed friction: {vel}")
+    return vel
+
+
 def main():
     pygame.init()
     clock = pygame.Clock()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    num_movers = random.randint(1, 10)
+    num_movers = random.randint(1, 5)
     #Creating walkers
     print(f"Created {num_movers} movers!")
     movers = []
 
     for _ in range(num_movers):
         h, w = 20, 20
-        x, y = random.randint(0, WIDTH - w), random.randint(0, HEIGHT - h)
+        x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+        mass = random.randint(1, 5)
         rand_color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-        curr_mover = moverObject.Mover(x, y, rand_color, h, w)
+        curr_mover = moverObject.Mover(x, y, rand_color, h, w, mass=mass)
         movers.append(curr_mover)
 
     screen.fill(BACKGROUND_COLOR)
@@ -64,14 +92,21 @@ def main():
 
         # movers are now subject to gravity
         for i, mover in enumerate(movers):
-            mover.apply_force(gravity)
+
+            # scale the gravity by the mover's mass
+            mover.apply_force(gravity * mover.get_mass())
             if is_wind_blowing:
                 mover.apply_force(wind)
 
+            if mover.check_floor(HEIGHT):
+                friction = compute_friction(mover)
+                mover.apply_force(friction)
             # position, velocity, acceleration, _, _, _ = mover.get_mover_attributes()
             # print(f"mover_{i} position: {position}, velocity: {velocity}, acceleration: {acceleration}")
             mover.update_position()
             mover.check_edges(WIDTH, HEIGHT)
+
+            
 
         screen.fill(BACKGROUND_COLOR)
         update_screen(screen, movers)
--- Forces/moverObject.py
+++ Forces/moverObject.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Mover():
-    def __init__(self, x: int, y: int, color: tuple, h: int, w: int, mass = 1, 
+    def __init__(self, x: int, y: int, color: tuple, h: int, w: int, elastiticy = -0.9, mass = 1, 
                 velocity = None, acceleration = None):
         
         
@@ -10,6 +10,7 @@ class Mover():
         self.w = w
         self.mass = mass
         
+        self.elastiticy = elastiticy
         # this are all pygame Vectors
         self.position = pygame.Vector2(x, y)
         self.velocity = velocity if velocity is not None else pygame.Vector2(0, 0)
@@ -25,7 +26,7 @@ class Mover():
         """
         
         f = force / self.mass  
-        self.acceleration += f  
+        self.acceleration += f 
 
     def update_position(self):
         """
@@ -38,7 +39,7 @@ class Mover():
 
     def check_edges(self, WIDTH: int, HEIGHT: int) -> None:
         """
-        Objects bounce of walls, this functions checks if a wall is hit.
+        Objects bounce of walls, this functions checks if a wall is hit (note that the floor is not considered a wall).
         If a wall is hit the velocity direction is inverted, and the position reset to wall's position.
 
         Args:
@@ -48,20 +49,46 @@ class Mover():
 
         if self.position.x > (WIDTH - self.w):
             self.position.x = WIDTH - self.w
-            self.velocity.x *= -1  
+            self.velocity.x *= self.elastiticy  
         elif self.position.x < 0:
             self.position.x = 0
-            self.velocity.x *= -1
+            self.velocity.x *= self.elastiticy  
 
-        if self.position.y > (HEIGHT - self.h):
-            self.position.y = HEIGHT - self.h
-            self.velocity.y *= -1
-        elif self.position.y < 0:
+        if self.position.y < 0:
             self.position.y = 0
-            self.velocity.y *= -1
+            self.velocity.y *= self.elastiticy  
+        elif self.position.y > (HEIGHT - self.h):
+            self.position.y = HEIGHT - self.h
+            self.velocity *= self.elastiticy
+        
+        
+    def check_floor(self, HEIGHT: int) -> bool:
+        """
+        Checks if floor is hit.
+
+        Args:
+            - HEIGHT -> height of the canvas in which objects are drawn
+
+        Returns True if floor is hit, False otherwise
+        """
+        if self.position.y >= (HEIGHT - self.h):
+            return True 
+        return False
 
     def get_mover_attributes(self):
         """
         Returns position and color of object, used mainly to draw the walker at each iteration.
         """
         return [self.position, self.velocity, self.acceleration, self.color, self.h, self.w] 
+    
+    def get_mass(self):
+        """
+        Returns the mover's mass.
+        """
+        return self.mass
+    
+    def get_velocity(self):
+        """
+        Returns the mover's velocity
+        """
+        return self.velocity
