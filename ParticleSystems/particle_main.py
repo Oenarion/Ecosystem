0b5f739fa9bf05e223da402af99eec2994818547
Lorenzo Galfano <41635328+Oenarion@users.noreply.github.com>
2025-04-15 15:03:47 +0200

    add ball death animation and emitters now die after a max capacity of particles is reached

--- ParticleSystems/particle_main.py
+++ ParticleSystems/particle_main.py
@@ -8,6 +8,8 @@ WIDTH = 640
 HEIGHT = 420
 BACKGROUND_COLOR = (0, 0, 0)
 TIME = time.time()
+MIN_PARTICLES = 10
+MAX_PARTICLES = 200
 
 
 
@@ -19,7 +21,8 @@ def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     screen.fill(BACKGROUND_COLOR)
-    first_emitter = particles.Emitter(WIDTH//2, 50)
+    first_emitter = particles.Emitter(WIDTH//2, 50, 
+                                      random.randint(MIN_PARTICLES, MAX_PARTICLES))
     gravity = pygame.Vector2(0, 0.1)
     running = True
     
@@ -35,12 +38,15 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
-                new_emitter = particles.Emitter(pos[0], pos[1])
+                new_emitter = particles.Emitter(pos[0], pos[1], 
+                                                random.randint(MIN_PARTICLES, MAX_PARTICLES))
                 emitters.append(new_emitter)
 
-        for emitter in emitters:
-            emitter.add_particle()
-            emitter.run(gravity, screen)
+        for i in range(len(emitters)-1, -1, -1):
+            emitters[i].add_particle()
+            emitters[i].run(gravity, screen)
+            if emitters[i].is_dead():
+                emitters.pop(i)
         
         # Update display
         pygame.display.update()
--- ParticleSystems/particles.py
+++ ParticleSystems/particles.py
@@ -33,6 +33,7 @@ class Particle():
         self.acceleration *= 0 
         self.rect = pygame.Rect(self.position.x - self.radius, self.position.y - self.radius, self.radius*2, self.radius*2)
 
+
     def draw(self, screen):
         """
         Draw the object on the canvas.
@@ -57,18 +58,22 @@ class Particle():
         self.draw(screen)
 
 class Emitter():
-    def __init__(self, origin_x, origin_y):
+    def __init__(self, origin_x, origin_y, max_particles):
         self.origin_position = pygame.Vector2(origin_x, origin_y)
         self.particles = []
+        self.max_particles = max_particles
 
     def add_particle(self):
         """
         Add a new particle to the emitter
         """
-        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.particles.append(Particle(self.origin_position.x, self.origin_position.y, color, 
-                                       4, velocity=pygame.Vector2(random.randint(-2, 2), random.randint(-4, 0))))
-        
+        if self.max_particles > 0:
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            self.particles.append(Particle(self.origin_position.x, self.origin_position.y, color, 
+                                        4, velocity=pygame.Vector2(random.randint(-2, 2), random.randint(-4, 0))))
+        self.max_particles -= 1
+
+
     def run(self, force, screen):
         """
         Apply force and draw the particles on the screen
@@ -77,3 +82,71 @@ class Emitter():
             self.particles[i].run(force, screen)
             if self.particles[i].is_dead():
                 self.particles.pop(i)
+
+    def is_dead(self):
+        """
+        Checks whether the emitter ended it's life cycle.
+        The life cycle is given by the number of particles created up to a maxium.
+        """
+        return self.max_particles <= 0 and len(self.particles) <= 0
+
+
+class ExplodingBall():
+    def __init__(self, x, y, radius, color, velocity = None, acceleration = None):
+        self.position = pygame.Vector2(x, y)
+        self.radius = radius
+        self.color = color
+        self.velocity = velocity if velocity is not None else pygame.Vector2(0, 0)
+        self.acceleration = acceleration if acceleration is not None else pygame.Vector2(0, 0)
+        self.particles = []
+        self.dead = False
+
+    def apply_force(self, force: pygame.Vector2):
+        """
+        Applies a force on the ball object (i.e. gravity), follows Newton's formula F = m x A
+
+        Args:
+            - force -> force to be applied
+        """
+        self.acceleration += force
+
+    def death_animation(self):
+        """
+        the ball explodes into smaller balls
+        """
+
+        for i in range(50):
+            self.particles.append(Particle(self.position.x, self.position.y, self.color, random.randint(1, self.radius//2),
+                                            pygame.Vector2(random.randint(-10, 10), random.randint(-10, 10))))
+
+    def update_position(self, HEIGHT):
+        """
+        Updates the position of the ball, used after a force is applied via apply_force().
+        """
+        if not self.dead:
+            self.velocity += self.acceleration
+            self.position += self.velocity
+            self.acceleration *= 0 
+            self.rect = pygame.Rect(self.position.x - self.radius, self.position.y - self.radius, self.radius*2, self.radius*2)
+            if self.position.y > HEIGHT and not self.dead:
+                self.dead = True
+                self.death_animation()
+        else:
+            for particle in self.particles:
+                gravity = pygame.Vector2(0, 0.1)
+                particle.apply_force(gravity)
+                particle.update_position()
+
+    def draw(self, screen):
+        """
+        Draws the ball on the screen.
+        """
+        if not self.dead:
+            pygame.draw.circle(screen, (128, 128, 128), self.position, self.radius + 2)
+            pygame.draw.circle(screen, self.color, self.position, self.radius)
+        else:
+            #draw particles
+            for particle in self.particles:
+                pygame.draw.circle(screen, (128, 128, 128), particle.position, particle.radius + 2)
+                pygame.draw.circle(screen, particle.color, particle.position, particle.radius)
+        
