b64eef292cf84c322e4298657aced9eb63763e99
oenarion <lorenzo.galfano00@gmail.com>
2025-06-17 07:52:41 +0200

    fixed small issue with snake movement

--- 9. Neuroevolution/snake.py
+++ 9. Neuroevolution/snake.py
@@ -24,6 +24,7 @@ def main():
     score = 0
     grid = Grid(WIDTH, HEIGHT, DIM)
     last_seen_move = ''
+    direction_chosen = False
     snake = Snake(31, 21, grid)
     grid.grid[31][21] = 'S'
     grid.add_food()
@@ -37,20 +38,25 @@ def main():
                 running = False 
             
             if event.type == pygame.KEYDOWN:
-                if (event.key == pygame.K_w or event.key == pygame.K_UP) and last_seen_move != 'S':
-                    last_seen_move = "W"
-                if (event.key == pygame.K_a or event.key == pygame.K_LEFT)and last_seen_move != 'D':
-                    last_seen_move = "A"
-                if (event.key == pygame.K_s or event.key == pygame.K_DOWN)and last_seen_move != 'W':
-                    last_seen_move = "S"
-                if (event.key == pygame.K_d or event.key == pygame.K_RIGHT) and last_seen_move != 'A':
-                    last_seen_move = "D"
-
+                if not direction_chosen:
+                    if (event.key == pygame.K_w or event.key == pygame.K_UP) and last_seen_move != 'S':
+                        last_seen_move = "W"
+                        direction_chosen = True
+                    if (event.key == pygame.K_a or event.key == pygame.K_LEFT)and last_seen_move != 'D':
+                        last_seen_move = "A"
+                        direction_chosen = True
+                    if (event.key == pygame.K_s or event.key == pygame.K_DOWN)and last_seen_move != 'W':
+                        last_seen_move = "S"
+                        direction_chosen = True
+                    if (event.key == pygame.K_d or event.key == pygame.K_RIGHT) and last_seen_move != 'A':
+                        last_seen_move = "D"
+                        direction_chosen = True
 
         # for i in range(len(grid)):
         #     for j in range(len(grid[0])):
         #         pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(i*DIM, j*DIM, DIM, DIM), width = 1)
 
+        direction_chosen = False
         snake.move(last_seen_move)
         eaten = snake.eat()
         if snake.check_hit():
@@ -63,7 +69,7 @@ def main():
         grid.draw(screen)
 
         score_text = FONT.render(f"SCORE: {score}", True, (200, 255, 0))
-        screen.blit(score_text, (WIDTH//2 - 100, 10))
+        screen.blit(score_text, (WIDTH//2 - 50, 10))
 
         # Update display
         pygame.display.update()
